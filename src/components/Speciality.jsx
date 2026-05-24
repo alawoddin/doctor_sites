b6380defdd96251b54a229c23e87127eaf2a15a1
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-24 11:49:01 +0430

    the same componet is add

---
+++ src/components/Speciality.jsx
@@ -0,0 +1,163 @@
+
+const Speciality = () => {
+    return (
+        <div>
+            <>
+  {/* Speciality Section */}
+  <section className="speciality-section">
+    <div className="container">
+      <div
+        className="section-header sec-header-one text-center aos"
+        data-aos="fade-up"
+      >
+        <span className="badge badge-primary">Top Specialties</span>
+        <h2>Highlighting the Care &amp; Support</h2>
+      </div>
+      <div className="owl-carousel spciality-slider aos" data-aos="fade-up">
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-01.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-01.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Cardiology</a>
+          </h6>
+          <p className="mb-0">254 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-02.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-02.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Orthopedics</a>
+          </h6>
+          <p className="mb-0">151 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-03.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-03.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Neurology</a>
+          </h6>
+          <p className="mb-0">176 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-04.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-04.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Pediatrics</a>
+          </h6>
+          <p className="mb-0">124 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-05.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-05.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Psychiatry</a>
+          </h6>
+          <p className="mb-0">112 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-06.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-06.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Endocrinology</a>
+          </h6>
+          <p className="mb-0">104 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-07.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-07.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Pulmonology</a>
+          </h6>
+          <p className="mb-0">41 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-08.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-08.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Urology</a>
+          </h6>
+          <p className="mb-0">39 Doctors</p>
+        </div>
+        <div className="spaciality-item">
+          <div className="spaciality-img">
+            <img src="assets/img/specialities/speciality-03.jpg" alt="img" />
+            <span className="spaciality-icon">
+              <img
+                src="assets/img/specialities/speciality-icon-03.svg"
+                alt="img"
+              />
+            </span>
+          </div>
+          <h6>
+            <a href="doctor-grid.html">Neurology</a>
+          </h6>
+          <p className="mb-0">176 Doctors</p>
+        </div>
+      </div>
+      <div className="spciality-nav nav-bottom owl-nav" />
+    </div>
+  </section>
+  {/* /Speciality Section */}
+</>
+
+        </div>
+    );
+};
+
+export default Speciality;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import Banner from '../components/Banner';
+import Doctor from '../components/Doctor';
 import Header from '../components/Header';
+import List from '../components/List';
 import Menu from '../components/Menu';
+import Services from '../components/Services';
+import Speciality from '../components/Speciality';
 // import Preloader from '../components/Preloader';
 
 const Home = () => {
@@ -9,6 +14,11 @@ const Home = () => {
   <div className="main-wrapper">
     <Header />
     <Menu />
+    <Banner />
+     <List />
+     <Speciality />
+     <Doctor />
+      <Services />
   </div>
 </>
 
