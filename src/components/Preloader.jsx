671987690334fdab70a3d4a4146a82a2944b569b
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-24 11:06:54 +0430

    the doctor site is start

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,19 @@
+const Preloader = () => {
+    return (
+        <div>
+            <>
+  {/* START PRELOADER AREA */}
+  <div className="preloader">
+    <div className="lds-ripple">
+      <div />
+      <div />
+    </div>
+  </div>
+  {/* END PRELOADER AREA */}
+</>
+
+        </div>
+    );
+};
+
+export default Preloader;
